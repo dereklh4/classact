ede9a4c2b21c29f24add7a63ac10b1207d8f2f77
Derek Hancock <derekhancock44@gmail.com>
2018-11-02 10:21:38 -0500

    add classroom join test case

--- backend/tests/tests.py
+++ backend/tests/tests.py
@@ -8,7 +8,7 @@ Run all tests with "python manage.py test"
 """
 
 from django.test import TestCase #use this instead of unittest.TestCase if interacting with model
-from classact_app.models import Classroom
+from classact_app.models import Classroom, UserInClassroom
 from django.contrib.auth.models import User
 from django.urls import include, path, reverse
 from rest_framework.test import APITestCase, URLPatternsTestCase
@@ -22,7 +22,7 @@ class UserTests(APITestCase):
 		"""
 		Ensure we can create a new account object.
 		"""
-		data = {'email': 'example_user@gmail.com',
+		data = {'email': 'bill_smith@gmail.com',
 				'password1':'f!re8all18',
 				'password2':'f!re8all18',
 				"first_name":"Bill",
@@ -33,12 +33,12 @@ class UserTests(APITestCase):
 		self.assertEqual(User.objects.count(), 1)
 		self.assertEqual(User.objects.get().first_name, 'Bill')
 		self.assertEqual(User.objects.get().last_name, 'Smith')
-		self.assertEqual(User.objects.get().email, 'example_user@gmail.com')
+		self.assertEqual(User.objects.get().email, 'bill_smith@gmail.com')
 
 class ClassroomTests(APITestCase):
 	def setUp(self):
 		#create and login user
-		data = {'email': 'example_user@gmail.com',
+		data = {'email': 'bill_smith@gmail.com',
 				'password1':'f!re8all18',
 				'password2':'f!re8all18',
 				"first_name":"Bill",
@@ -57,3 +57,30 @@ class ClassroomTests(APITestCase):
 		self.assertEqual(Classroom.objects.count(), 1)
 		self.assertEqual(Classroom.objects.get().enabled, True)
 		self.assertEqual(Classroom.objects.get().title, "software engineering")
+		self.assertEqual(UserInClassroom.objects.get().permission, 3) #creator level permission
+
+	def test_classroom_join(self):
+		#create classroom as Bill
+		data = {'title': 'software engineering'}
+		response = self.client.post(PREFIX + 'classroom/', data, format='json')
+		classroom_url = Classroom.objects.get().url
+
+		#create/login as a different user
+		data = {'email': 'john_williams@gmail.com',
+				'password1':'f!re8all18',
+				'password2':'f!re8all18',
+				"first_name":"John",
+				"last_name":"Williams"
+				}
+		response = self.client.post(PREFIX + 'auth/registration/', data, format='json')
+
+		#join classroom as different user
+		data2 = {"url":classroom_url}
+		response = self.client.post(PREFIX + 'classroom/join/', data2, format='json')
+
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(UserInClassroom.objects.count(), 2)
+		self.assertEqual(UserInClassroom.objects.get(pk=2).user.first_name, "John")
+		self.assertEqual(UserInClassroom.objects.get(pk=2).classroom.url, classroom_url)
+		self.assertEqual(UserInClassroom.objects.get(pk=2).permission, 1)
+
